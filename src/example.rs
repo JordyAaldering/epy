469bc3fc8e8c731e1c97f958b919a1b873f2745e
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-05-08 17:22:31 +0200

    fix grid arrow

--- src/example.rs
+++ src/example.rs
@@ -73,9 +73,7 @@ fn twin(df: &DataFrame<Record>) {
     doc.ax0.set_ymax(0.1);
     doc.ax0.filter_xticks_stride(2);
     doc.ax0.format_xticks_precision(1);
-    if let Some(ax1) = &mut doc.ax1 {
-        ax1.set_ymax(0.6);
-    }
+    doc.ax1.as_mut().unwrap().set_ymax(0.5);
 
     let tikz = doc.render_tikz();
 
